183ed3f803a9ca03b181732cc1fc104f398d0b13
abhi12rai <abhisekhrai3@gmail.com>
2025-04-25 01:03:00 +0530

    Update food_delivery_analysis.sql

--- food_delivery_analysis.sql
+++ food_delivery_analysis.sql
@@ -1,1 +1,10 @@
-
+-- 1. Find the top 3 outlets by cuisine type without using the LIMIT and TOP functions.
+WITH CTE AS (
+    SELECT Cuisine, Restaurant_Id, COUNT(*) AS NumOrders,
+           ROW_NUMBER() OVER (PARTITION BY Cuisine ORDER BY COUNT(*) DESC) AS RN
+    FROM Orders
+    GROUP BY Cuisine, Restaurant_Id
+)
+SELECT Cuisine, Restaurant_Id
+FROM CTE
+WHERE RN <=3;
