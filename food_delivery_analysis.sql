98830e5b1b3957c0888ea20d931c653005bb964c
abhi12rai <abhisekhrai3@gmail.com>
2025-04-25 01:07:30 +0530

    Update food_delivery_analysis.sql

--- food_delivery_analysis.sql
+++ food_delivery_analysis.sql
@@ -8,3 +8,11 @@ WITH CTE AS (
 SELECT Cuisine, Restaurant_Id
 FROM CTE
 WHERE RN <=3;
+
+**Explanation:**
+1. I use a CTE (Common Table Expression) to get the count of orders for each Restaurant_ID within each Cuisine type.
+2. Within the CTE, we also generate the row number for each Restaurant_ID within each Cuisine, 
+   ordered by the number of orders in descending order.
+3. Finally, we select the rows where the row number is <=3 to get the top 3 outlet for each Cuisine. 
+   This way, we can get the top 3 (or any number) outlets for each Cuisine type without using the LIMIT or TOP functions.
+======================================================================================================================================================================================================================
