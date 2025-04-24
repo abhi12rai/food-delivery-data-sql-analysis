eac7d98b5171a23ebf4e6589356e3199ee462599
abhi12rai <abhisekhrai3@gmail.com>
2025-04-25 01:33:59 +0530

    Update food_delivery_analysis.sql

--- food_delivery_analysis.sql
+++ food_delivery_analysis.sql
@@ -16,3 +16,23 @@ WHERE RN <=3;
 3. Finally, we select the rows where the row number is <=3 to get the top 3 outlet for each Cuisine. 
    This way, we can get the top 3 (or any number) outlets for each Cuisine type without using the LIMIT or TOP functions.
 =============================================================================================================================
+
+-- 2.Find the daily new customer count from the launch date(how many new customers joined each day)
+with cte as (
+select Customer_code,cast(min(Placed_at) as date) as
+ first_order_date from orders
+group by Customer_code)
+select first_order_date,count(Customer_code) as no_of_new_customer
+from cte
+group by first_order_date
+order by first_order_date;
+=================================================================================================================================
+-- 3.Count of all users who were acquired in January 2025 and only placed one order in January and did not placed any other order
+with cte as (
+select Customer_code,count(*)as no_of_order from orders
+where month(placed_at)=1 and year(placed_at)=2025
+group by customer_code
+having no_of_order =1)
+select customer_code from cte 
+where customer_code not in (select distinct Customer_code from orders
+where month(placed_at) in (2,3));
