d4edfc75e9036b80c707ead6b87894d3af160e06
Abhishek Rai <abhisekhrai3@gmail.com>
2025-04-26 19:03:57 +0530

    Delete 6_customers_only_promo_orders.sql

--- 6_customers_only_promo_orders.sql
+++
@@ -1,8 +0,0 @@
--- 📋 Problem 6: Customers Who Placed More Than One Orders Using Promo Codes
-
-select Customer_code,count(order_id) as no_of_order,
-count(Promo_code_Name) as promo_order
-from orders
-group by Customer_code
-having no_of_order>1 and promo_order=no_of_order;
-
