81a449195a8802453c5b858374a918f7779d9127
Abhishek Rai <abhisekhrai3@gmail.com>
2025-04-26 17:15:39 +0530

    Update 📄 3_customers_one_order_in_jan2025.sql

--- 📄 3_customers_one_order_in_jan2025.sql
+++ 📄 3_customers_one_order_in_jan2025.sql
@@ -1,25 +1,14 @@
 -- 📋 Problem 3: Find Customers Acquired in Jan 2025 Who Only Placed One Order
 
--- Step 1: Find each customer's first order date
-WITH CustomerOrders AS (
-    SELECT 
-        Customer_code,
-        MIN(CAST(Placed_at AS DATE)) AS FirstOrderDate,
-        COUNT(Order_id) AS TotalOrders
-    FROM 
-        orders
-    GROUP BY 
-        Customer_code
-)
-
--- Step 2: Filter customers acquired in Jan 2025 with only one order
-SELECT 
-    Customer_code
-FROM 
-    CustomerOrders
-WHERE 
-    FirstOrderDate BETWEEN '2025-01-01' AND '2025-01-31'
-    AND TotalOrders = 1;
+select Customer_code,count(*) as no_of_order 
+from orders
+where month(placed_at)=1 and year(placed_at)=2025 
+and Customer_code not in (select distinct Customer_code
+from orders
+where not (month(placed_at)=1 and year(placed_at)=2025)) 
+group by customer_code
+having count(*) =1
+order by Customer_code;
 
 -- Insight:
 -- Lists customers acquired in January 2025 who placed only one order and did not order again.
