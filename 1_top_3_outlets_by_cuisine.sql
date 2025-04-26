2fbe56ea70241e26b22a0294196e540c5679059e
Abhishek Rai <abhisekhrai3@gmail.com>
2025-04-26 17:03:39 +0530

    Update 1_top_3_outlets_by_cuisine.sql

--- 1_top_3_outlets_by_cuisine.sql
+++ 1_top_3_outlets_by_cuisine.sql
@@ -1,30 +1,15 @@
 -- 📋 Problem 1: Find the Top 3 Outlets by Cuisine Type
 -- Without using LIMIT or TOP functions.
 
--- Step 1: Create a CTE to calculate order count per restaurant within each cuisine
-WITH CuisineOutletRank AS (
-    SELECT 
-        Cuisine,
-        Restaurant_id,
-        COUNT(*) AS TotalOrders,
-        ROW_NUMBER() OVER (PARTITION BY Cuisine ORDER BY COUNT(*) DESC) AS rn
-    FROM 
-        orders
-    GROUP BY 
-        Cuisine, Restaurant_id
-)
-
--- Step 2: Select top 3 outlets per cuisine based on the rank
-SELECT 
-    Cuisine,
-    Restaurant_id,
-    TotalOrders
-FROM 
-    CuisineOutletRank
-WHERE 
-    rn <= 3
-ORDER BY 
-    Cuisine, rn;
+WITH CTE AS (
+    SELECT Cuisine, Restaurant_Id, COUNT(*) AS no_of_orders
+from orders
+group by Cuisine, Restaurant_id)
+select * from ( 
+			select *,
+           ROW_NUMBER() OVER (PARTITION BY cuisine ORDER BY no_of_orders DESC) AS rn
+    FROM cte) a
+WHERE RN <=3;
 
 -- Insight:
 -- Shows the top 3 performing restaurants within each cuisine based on order volume.
